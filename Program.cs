55f957a6eea9e040a167daf72641e1462057b320
Bob Giwleb <junk@hutchinthehills.com>
2021-12-08 15:12:09 -0500

    I think I got it

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SpotifyAPI.Web;
+
+Console.WriteLine("Start");
+
+var config = SpotifyClientConfig.CreateDefault();
+var request = new ClientCredentialsRequest("x", "x");
+var response = await new OAuthClient(config).RequestToken(request);
+var spotify = new SpotifyClient(config.WithToken(response.AccessToken));
+
+var xmasMegaPlId = "0BBjPYvDcKOUF1P5GRB8W7";
+
+var pldata = await spotify.Playlists.GetItems(xmasMegaPlId);
+
+var items = await spotify.Paginate(pldata).ToListAsync();
+var tracks = items.Select(i => i.Track).OfType<FullTrack>();
+var maxArtists = tracks.Max(x => x.Artists.Count);
+
+foreach(var track in tracks)
+{  
+    string row = $"{track.Name},{track.Album.Name}";
+    var artistsCount = track.Artists.Count;
+
+    for (int i = 1; i <= track.Artists.Count; i++)
+    {
+        row += $",{track.Artists[i-1].Name}";
+    }
+    for (int i = track.Artists.Count + 1; i <= maxArtists; i++)
+    {
+        row += ",";
+    }
+
+    Console.WriteLine(row);    
+}  
+
+
+
+Console.WriteLine("End");
